f2afe226a27e97d89a4ea2e340adb68a86a86002
Akshay <akshaydevadiga4@gmail.com>
2022-01-17 01:44:21 +0530

    Implement Not Found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import NotFound from "./components/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Image, Button, Spacer, Text } from "@chakra-ui/react";
+import NotFoundImage from "../404.png";
+import { BsChevronLeft } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+const NotFound = (props) => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate("/");
+  };
+  return (
+    <Box align="center" p="4" justify="center">
+      <Image boxSize="40vh" src={NotFoundImage} />
+      <Box display="inline-block">
+        <Text>Page not found</Text>
+        <Button
+          p="4"
+          leftIcon={<BsChevronLeft />}
+          colorScheme="primary.900"
+          variant="ghost"
+          onClick={goBack}
+        >
+          Back
+        </Button>
+        <Spacer />
+      </Box>
+    </Box>
+  );
+};
+
+NotFound.propTypes = {};
+
+export default NotFound;
