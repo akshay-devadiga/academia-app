e8580f350d24b51704512998351f552b9b126ecf
Akshay <akshaydevadiga4@gmail.com>
2022-01-17 01:51:21 +0530

    Implement route navigation

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import Content from "./components/Content";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import NotFound from "./components/NotFound";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Outlet,Navigate } from "react-router-dom";
 function App() {
+  function RequireAuth() {
+    let auth = localStorage.getItem('accessToken');
+    if (!auth) {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      return <Navigate to="/"  />;
+    }
+  
+    return <Outlet />;
+  }
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route element={<RequireAuth />}>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/student" element={<Dashboard />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
