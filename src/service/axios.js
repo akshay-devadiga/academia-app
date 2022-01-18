f7aff61dad124ac1d60452de9d51b8321f5549e3
Akshay <akshaydevadiga4@gmail.com>
2022-01-18 20:34:35 +0530

    Update production api url

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NODE_ENV ? "https://academia-server.herokuapp.com/api"  :"http://localhost:3000/api",
 });
 instance.interceptors.request.use(
   function (config) {
